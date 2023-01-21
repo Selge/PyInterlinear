eb77e429418ccf46671664ae0411859a7ddf82fe
Sergei Shcherbak <sshch1987@gmail.com>
2023-01-21 11:34:56 +0200

    Update translator.py

--- translator.py
+++ translator.py
@@ -31,28 +31,38 @@ def start_menu():
 def read_input():
     raw_text = str(input("Please, enter your target text here:  "))
 
-    return raw_text
+    language_detect(raw_text)
 
 
 def read_from_file():
     raw_text = ''
 
-    return raw_text
+    language_detect(raw_text)
 
 
 def language_detect(raw_text):
     lang_suppose = detect(raw_text)
 
-    return lang_suppose
-
-
-def write_to_file():
-    ...
+    translate(raw_text, lang_suppose)
 
 
 def translate(alien_text, lang_suppose, dest=TARGET_LANG):
     translated_text = translator.translate(alien_text, src=lang_suppose, dest=dest)
-    return translated_text
+
+    print("""Please, choose an output option:
+             - 'f' - to write the result to file;
+             - 'p' - to print the result to console;        
+          """)
+
+    write_to_file(translated_text)
+
+
+def write_to_file(translated_text):
+    ...
+
+
+def print_result(translated_text):
+    print(translated_text)
 
 
 if __name__ == '__main__':
